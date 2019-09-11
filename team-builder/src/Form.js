910704da2ac3e91b37eb72c5fe04214f95734b6f
Jesse-Tingle <jtingle0@gmail.com>
2019-09-11 01:56:59 -0400

    name field renders to screen and updates state value

--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Form = props => {
-    return <></>
+    const [name, setName] = useState();
+
+    const handleChange = (event) => {
+        setName(event.target.value);
+    }
+
+    return (
+        <form>
+            Name: {name}
+            <input
+                onChange={handleChange}
+                type="text" 
+                name="name"
+                placeholder="Name"
+            />
+
+            {/* <input
+                type="text"
+                name="email"
+                placeholder="Email"
+            />
+
+            <select>
+                <option>Full Stack Engineer</option>
+                <option>Front End Engineer</option>
+                <option>Backend Engineer</option>
+                <option>UX/UI Engineer</option>
+                <option>Data Science Engineer</option>
+            </select> */}
+
+            <button type="submit">Submit</button>
+        </form>
+    )
 }
 
 export default Form;
